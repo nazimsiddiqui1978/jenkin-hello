54e495083032615e5ee4d9c29fe51e7fd3c32d00
nazimsiddiqui1978 <nazim31s@gmail.com>
2025-03-24 14:35:19 -0400

    Update HelloWorld.java
    nazim_31s

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 public static void main(String[] args){
-System.out.println(" -----***** Hello World & welcome to nazim first program of java******Nazim----");
+System.out.println(" -----***** Hello World & welcome to nazim first program of java******Nazim_31s----");
 }
 }
